757f1206e614b69af252acdaa124890afe9db9d7
stepan <stsalamta@gmail.com>
2023-09-17 13:14:13 +0300

    add and setting eureka service

--- customer/src/main/java/ru/chemakin/customer/CustomerService.java
+++ customer/src/main/java/ru/chemakin/customer/CustomerService.java
@@ -21,13 +21,13 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
 
 
-        if(fraudCheckResponse.isFraudster()){
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
     }
